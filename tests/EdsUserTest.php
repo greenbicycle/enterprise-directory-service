ac9e7398bc97b0a45664d81c6c5fdeb1d75c8eab
Jeff Davis <jeff@greenbicycle.net>
2020-07-09 12:05:13 -0700

    Changed toplevel namespace to EnterpriseDirectoryServices

--- tests/EdsUserTest.php
+++ tests/EdsUserTest.php
@@ -2,70 +2,70 @@
 declare(strict_types=1);
 
 use PHPUnit\Framework\TestCase;
-use EdsUser\EdsUserInfo;
+use EnterpriseDirectoryService\EdsUser;
 
-class EdsUserInfoTest extends TestCase
+class EdsUserTest extends TestCase
 {
-    public $edsUserInfo;
+    public $edsUser;
     public $extraAttributes;
 
     public function setUp() : void
     {
-        $this->edsUserInfo = new EdsUserInfo();
+        $this->edsUser = new EdsUser();
         $this->extraAttributes = [
           'statusHistory' => 'studentStatusHistory'
         ];
     }
     public function testThatClassExists()
     {
-        $this->assertTrue(class_exists(EdsUserInfo::class));
+        $this->assertTrue(class_exists(EdsUser::class));
     }
 
     public function testThatOptionsCanBeSetFromEnvironment()
     {
         // This should fail. Options haven't been set yet.
         $this->assertFalse(
-            $this->edsUserInfo->validate()
+            $this->edsUser->validate()
         );
 
         $this->assertTrue(
-            $this->edsUserInfo->setOptionsFromEnvironment()
+            $this->edsUser->setOptionsFromEnvironment()
                 ->validate()
         );
     }
 
     public function testThatAttributeCanBeQueried()
     {
-        $this->edsUserInfo->setOptionsFromEnvironment()
+        $this->edsUser->setOptionsFromEnvironment()
             ->requestResponse(getenv('EDS_TEST_ID'));
         $this->assertTrue(
-            count($this->edsUserInfo->queryAttribute('uid')) > 0
+            count($this->edsUser->queryAttribute('uid')) > 0
         );
         $this->assertTrue(
-            count($this->edsUserInfo->queryAttribute('cn')) > 0
+            count($this->edsUser->queryAttribute('cn')) > 0
         );
     }
     public function testThatNonexistentAttributeCannotBeQueried()
     {
-        $this->edsUserInfo->setOptionsFromEnvironment()
+        $this->edsUser->setOptionsFromEnvironment()
             ->requestResponse(getenv('EDS_TEST_ID'));
-        $this->assertEquals(0, count($this->edsUserInfo->queryAttribute('foo')));
+        $this->assertEquals(0, count($this->edsUser->queryAttribute('foo')));
     }
 
     public function testThatGetAllAttributesMethodWorks()
     {
-        $this->edsUserInfo->setOptionsFromEnvironment()
+        $this->edsUser->setOptionsFromEnvironment()
             ->requestResponse(getenv('EDS_TEST_ID'));
         $this->assertTrue(
-            count($this->edsUserInfo->getAllAttributes()) > 0
+            count($this->edsUser->getAllAttributes()) > 0
         );
     }
     public function testThatGetAllAttributesMethodWorksWithFirstOnly()
     {
-        $this->edsUserInfo->setOptionsFromEnvironment()
+        $this->edsUser->setOptionsFromEnvironment()
             ->requestResponse(getenv('EDS_TEST_ID'));
         $this->assertTrue(
-            count($this->edsUserInfo->getAllAttributes(true)) > 0
+            count($this->edsUser->getAllAttributes(true)) > 0
         );
     }
 
@@ -74,13 +74,13 @@ class EdsUserInfoTest extends TestCase
      */
     public function testThatExtraAttributesCanBeAdded()
     {
-        $this->edsUserInfo->setOptionsFromEnvironment()
+        $this->edsUser->setOptionsFromEnvironment()
             ->addAttributes($this->extraAttributes)
             ->requestResponse(getenv('EDS_TEST_ID'));
         $this->assertTrue(
             array_key_exists(
                 array_key_first($this->extraAttributes),
-                $this->edsUserInfo->getAllAttributes()
+                $this->edsUser->getAllAttributes()
             )
         );
     }
